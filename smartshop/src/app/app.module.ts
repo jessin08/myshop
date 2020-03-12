f3ad6d74361e06737c1f8383b0fffc2f9a4f7bde
Jessin <jessinrules@gmail.com>
2020-03-12 12:53:08 +0100

    Added home,navbar,registration,and layout modules

--- smartshop/src/app/app.module.ts
+++ smartshop/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Import Feature Modules
+import { LayoutModule } from './layout/layout.module';
+import { HomeModule } from './feature/home/home.module';
+import { RegistrationModule } from './feature/registration/registration.module';
+import { NavbarModule } from './feature/navbar/navbar.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+// import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SignupComponent,
-    NavbarComponent
+    // NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    HomeModule,
+    RegistrationModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- smartshop/src/app/feature/registration/registration.module.ts
+++ smartshop/src/app/feature/registration/registration.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
 
 
@@ -7,7 +8,8 @@ import { RegistrationComponent } from './registration.component';
 @NgModule({
   declarations: [RegistrationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RegistrationRoutingModule
   ]
 })
 export class RegistrationModule { }
